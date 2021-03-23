a32a4457a40cc9812198b12dde0249967089a83e
kartik <kavimandankartik33@gmail.com>
2021-03-23 16:45:43 -0700

    Added: PartTime emoloyee wage

--- EMPLOYEE/Empwage.java
+++ EMPLOYEE/Empwage.java
@@ -2,25 +2,30 @@ public class Empwage {
 
         public static void main(String[] args) {
         int isfullTime=1;
+	int ispartTime=2;
         double employeeRateperHr=20;
-        double employeeHr=8;
-        double Dailyemployeewage;
+        double fullTimeemployeeHr=8;
+	double partTimeemployeeHr=4;
         double empwage;
-        double empCheck=Math.floor(Math.random() * 10) %2 ;
-
-        if (empCheck == isfullTime)
-                {
-                empwage=employeeHr*employeeRateperHr;
-                System.out.println("Dailyemployeewage:" +empwage);
-                }
-        else
+        double empCheck=Math.floor(Math.random() * 10) %3;
         
- {
-                  System.out.println("Dailyemployeewage:" +0);
+	if
+		(empCheck == isfullTime)
+                {
+                empwage=fullTimeemployeeHr*employeeRateperHr;
+                System.out.println("fullTimeEmpwage:" +empwage);
                 }
-        }
+        else if
+		 (empCheck == ispartTime)
+
+		{
+		empwage=partTimeemployeeHr*employeeRateperHr;
+                System.out.println("partTimeEmpwage:" +empwage );
+		}
+	else
+		{
+		System.out.println("Employeeis absent" );
+		}
+	}
 }
-
-
-
 
