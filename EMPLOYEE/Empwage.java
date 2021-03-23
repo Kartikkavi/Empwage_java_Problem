2020e76697510f314dbddd39830e659f55719c86
kartik <kavimandankartik33@gmail.com>
2021-03-23 16:51:03 -0700

    Added: Switch case

--- EMPLOYEE/Empwage.java
+++ EMPLOYEE/Empwage.java
@@ -1,31 +1,32 @@
 public class Empwage {
 
         public static void main(String[] args) {
-        int isfullTime=1;
-	int ispartTime=2;
+        double isfullTime=1;
+	double ispartTime=2;
+	double partempHr=4;
+        double fullempHr=8;
         double employeeRateperHr=20;
-        double fullTimeemployeeHr=8;
-	double partTimeemployeeHr=4;
         double empwage;
         double empCheck=Math.floor(Math.random() * 10) %3;
-        
-	if
-		(empCheck == isfullTime)
-                {
-                empwage=fullTimeemployeeHr*employeeRateperHr;
-                System.out.println("fullTimeEmpwage:" +empwage);
-                }
-        else if
-		 (empCheck == ispartTime)
+	        switch ((int) empCheck) 
+                        {
+		case 1:
+			if 
+			(empCheck == isfullTime){
+			empwage=fullempHr*employeeRateperHr;
+			System.out.println("FullTime:" +empwage);}
+		break;
 
-		{
-		empwage=partTimeemployeeHr*employeeRateperHr;
-                System.out.println("partTimeEmpwage:" +empwage );
+		case 2:
+			if 
+			(empCheck == ispartTime){
+		        empwage=partempHr*employeeRateperHr;
+                        System.out.println("PartTime:" +empwage);}
+                break;
+
+		default: 
+			{
+			System.out.println("absent");}
+			}
 		}
-	else
-		{
-		System.out.println("Employeeis absent" );
-		}
-	}
 }
-
